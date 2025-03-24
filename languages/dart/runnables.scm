bc307c4c42491e51d9193cc2e5293a65651ea651
arnaud-secondlayer <87088015+arnaud-secondlayer@users.noreply.github.com>
2025-03-24 12:41:50 -0500

    Add tasks for tests (#7)
    This PR create runnable buttons for dart unit tests (non-flutter). It
    adds buttons for:
    * the whole file, on `main` function
    * each `group`
    * each `test`
    
    RESOLVES: #9

--- languages/dart/runnables.scm
+++ languages/dart/runnables.scm
@@ -43,3 +43,79 @@
         (#set! tag flutter-test-main)
     )
 )
+
+; Dart test file
+(
+    (
+        (import_or_export
+            (library_import
+                (import_specification
+                    ("import"
+                        (configurable_uri
+                            (uri
+                                (string_literal) @_import
+                                (#match? @_import "package:test/test.dart")
+        ))))))
+        (
+            (function_signature
+                name: (_) @run
+            )
+            (#eq? @run "main")
+        )
+        (#set! tag dart-test-file)
+    )
+)
+
+; Dart test group
+(
+    (
+        (import_or_export
+            (library_import
+                (import_specification
+                    ("import"
+                        (configurable_uri
+                            (uri
+                                (string_literal) @_import
+                                (#match? @_import "package:test/test.dart")
+        ))))))
+        (
+            (function_body
+                (block
+                    (expression_statement
+                        (
+                            (identifier) @run (#eq? @run "group")
+        )))))
+        (#set! tag dart-test-group)
+    )
+)
+
+; Dart test single
+(
+    (
+        (import_or_export
+            (library_import
+                (import_specification
+                    ("import"
+                        (configurable_uri
+                            (uri
+                                (string_literal) @_import
+                                (#match? @_import "package:test/test.dart")
+        ))))))
+        (
+            (function_body
+                (block
+                    (expression_statement
+                        (selector
+                            (argument_part
+                                (arguments
+                                    (argument
+                                        (function_expression
+                                            (function_expression_body
+                                                (block
+                                                    (expression_statement
+                                                        (
+                                                            (identifier) @run (#eq? @run "test")
+        )))))))))))))
+        (#set! tag dart-test-single)
+    )
+)
