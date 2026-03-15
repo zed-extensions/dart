853d48265d358a8734b94e68979ba6d5a6706bed
MrSubidubi <dev@bahn.sh>
2026-03-15 22:09:13 +0100

    Format Tree-sitter query files

--- languages/dart/highlights.scm
+++ languages/dart/highlights.scm
@@ -1,100 +1,110 @@
 (identifier) @variable
+
 (dotted_identifier_list) @string
 
 ; Methods
 ; --------------------
 (super) @function
 
-(function_expression_body (identifier) @type)
-; ((identifier)(selector (argument_part)) @function)
+(function_expression_body
+  (identifier) @type)
 
-(((identifier) @function (#match? @function "^_?[a-z]"))
- . (selector . (argument_part))) @function
+; ((identifier)(selector (argument_part)) @function)
+(((identifier) @function
+  (#match? @function "^_?[a-z]"))
+  .
+  (selector
+    .
+    (argument_part))) @function
 
 ; Annotations
 ; --------------------
 (annotation
- name: (identifier) @attribute)
+  name: (identifier) @attribute)
 
 ; Operators and Tokens
 ; --------------------
 (template_substitution
- "$" @punctuation.special
- "{" @punctuation.special
- "}" @punctuation.special) @none
+  "$" @punctuation.special
+  "{" @punctuation.special
+  "}" @punctuation.special) @none
 
 (template_substitution
- "$" @punctuation.special
- (identifier_dollar_escaped) @variable) @none
+  "$" @punctuation.special
+  (identifier_dollar_escaped) @variable) @none
 
 (escape_sequence) @string.escape
 
 [
- "@"
- "=>"
- ".."
- "??"
- "=="
- "?"
- ":"
- "&&"
- "%"
- "<"
- ">"
- "="
- ">="
- "<="
- "||"
- (multiplicative_operator)
- (increment_operator)
- (is_operator)
- (prefix_operator)
- (equality_operator)
- (additive_operator)
- ] @operator
+  "@"
+  "=>"
+  ".."
+  "??"
+  "=="
+  "?"
+  ":"
+  "&&"
+  "%"
+  "<"
+  ">"
+  "="
+  ">="
+  "<="
+  "||"
+  (multiplicative_operator)
+  (increment_operator)
+  (is_operator)
+  (prefix_operator)
+  (equality_operator)
+  (additive_operator)
+] @operator
 
 [
- "("
- ")"
- "["
- "]"
- "{"
- "}"
- ] @punctuation.bracket
+  "("
+  ")"
+  "["
+  "]"
+  "{"
+  "}"
+] @punctuation.bracket
 
 ; Delimiters
 ; --------------------
 [
- ";"
- "."
- ","
- ] @punctuation.delimiter
+  ";"
+  "."
+  ","
+] @punctuation.delimiter
 
 ; Types
 ; --------------------
 (class_definition
- name: (identifier) @type)
+  name: (identifier) @type)
+
 (constructor_signature
- name: (identifier) @type)
+  name: (identifier) @type)
+
 (scoped_identifier
- scope: (identifier) @type)
+  scope: (identifier) @type)
+
 (function_signature
- name: (identifier) @function.method)
+  name: (identifier) @function.method)
 
 (enum_declaration
- name: (identifier) @type)
+  name: (identifier) @type)
+
 (enum_constant
- name: (identifier) @property)
+  name: (identifier) @property)
 
 ((scoped_identifier
   scope: (identifier) @type
   name: (identifier) @type)
- (#match? @type "^[a-zA-Z]"))
+  (#match? @type "^[a-zA-Z]"))
 
 (type_identifier) @type
 
 (type_alias
- (type_identifier) @type.definition)
+  (type_identifier) @type.definition)
 
 ; Variables
 ; --------------------
@@ -102,79 +112,97 @@
 (inferred_type) @keyword
 
 ((identifier) @type
- (#match? @type "^_?[A-Z].*[a-z]"))
+  (#match? @type "^_?[A-Z].*[a-z]"))
 
 ; properties
 (unconditional_assignable_selector
- (identifier) @property)
+  (identifier) @property)
 
 (conditional_assignable_selector
- (identifier) @property)
+  (identifier) @property)
 
 (cascade_selector
- (identifier) @property)
+  (identifier) @property)
 
 (getter_signature
- (identifier) @property)
+  (identifier) @property)
+
 (setter_signature
- name: (identifier) @property)
+  name: (identifier) @property)
 
 ((selector
-  (unconditional_assignable_selector (identifier) @function.method))
-  . (selector (argument_part (arguments)))
-)
+  (unconditional_assignable_selector
+    (identifier) @function.method))
+  .
+  (selector
+    (argument_part
+      (arguments))))
 
 ((selector
-  (conditional_assignable_selector (identifier) @function.method))
-  . (selector (argument_part (arguments)))
-)
+  (conditional_assignable_selector
+    (identifier) @function.method))
+  .
+  (selector
+    (argument_part
+      (arguments))))
 
-((cascade_section
-  (cascade_selector (identifier) @function.method)
-  . (argument_part (arguments)))
-)
+(cascade_section
+  (cascade_selector
+    (identifier) @function.method)
+  .
+  (argument_part
+    (arguments)))
 
 ; Some methods do not have a selector as a parent of the conditional_assignable_selector
 ; For example, super methods.
-((unconditional_assignable_selector (identifier) @function.method)
-  . (selector (argument_part (arguments)))
-)
+((unconditional_assignable_selector
+  (identifier) @function.method)
+  .
+  (selector
+    (argument_part
+      (arguments))))
 
-((conditional_assignable_selector (identifier) @function.method)
-  . (selector (argument_part (arguments)))
-)
+((conditional_assignable_selector
+  (identifier) @function.method)
+  .
+  (selector
+    (argument_part
+      (arguments))))
 
 ; assignments
 (assignment_expression
- left: (assignable_expression) @variable)
+  left: (assignable_expression) @variable)
 
 (this) @variable.builtin
 
 ; Parameters
 ; --------------------
 (formal_parameter
- (identifier) @variable.parameter)
+  (identifier) @variable.parameter)
 
 (named_argument
- (label
+  (label
     (identifier) @variable.parameter))
 
 ; Literals
 ; --------------------
 [
- (hex_integer_literal)
- (decimal_integer_literal)
- (decimal_floating_point_literal)
- ; TODO: inaccessible nodes
- ; (octal_integer_literal)
- ; (hex_floating_point_literal)
- ] @number
+  (hex_integer_literal)
+  (decimal_integer_literal)
+  (decimal_floating_point_literal)
+  ; TODO: inaccessible nodes
+  ; (octal_integer_literal)
+  ; (hex_floating_point_literal)
+] @number
 
 (symbol_literal) @string.special.symbol
 
 (string_literal) @string
+
 (true) @boolean
+
 (false) @boolean
+
 (null_literal) @constant.builtin
 
 (comment) @comment
@@ -184,51 +212,51 @@
 ; Keywords
 ; --------------------
 [
- "import"
- "library"
- "export"
- "as"
- "show"
- "hide"
- ] @keyword.import
+  "import"
+  "library"
+  "export"
+  "as"
+  "show"
+  "hide"
+] @keyword.import
 
 ; Reserved words (cannot be used as identifiers)
 [
   (case_builtin)
   (void_type)
- "late"
- "required"
- "extension"
- "on"
- "class"
- "enum"
- "extends"
- "in"
- "is"
- "new"
- "super"
- "with"
- "Function"
- ] @keyword.definition
+  "late"
+  "required"
+  "extension"
+  "on"
+  "class"
+  "enum"
+  "extends"
+  "in"
+  "is"
+  "new"
+  "super"
+  "with"
+  "Function"
+] @keyword.definition
 
 "return" @keyword.return
 
 ; Built in identifiers:
 ; alone these are marked as keywords
 [
- (part_of_builtin)
- "deferred"
- "factory"
- "get"
- "implements"
- "interface"
- "library"
- "operator"
- "mixin"
- "part"
- "set"
- "typedef"
- ] @keyword
+  (part_of_builtin)
+  "deferred"
+  "factory"
+  "get"
+  "implements"
+  "interface"
+  "library"
+  "operator"
+  "mixin"
+  "part"
+  "set"
+  "typedef"
+] @keyword
 
 [
   "async"
@@ -239,41 +267,23 @@
 ] @keyword.coroutine
 
 [
- (const_builtin)
- (final_builtin)
- "abstract"
+  (const_builtin)
+  (final_builtin)
+  "abstract"
   "covariant"
- "dynamic"
- "external"
- "static"
- "final"
+  "dynamic"
+  "external"
+  "static"
+  "final"
   "base"
   "sealed"
- ] @keyword.modifier ; could be @type.qualifier
+] @keyword.modifier ; could be @type.qualifier
 
 ; when used as an identifier:
 ((identifier) @variable.builtin
- (#any-of? @variable.builtin
-           "abstract"
-           "as"
-           "covariant"
-           "deferred"
-           "dynamic"
-           "export"
-           "external"
-           "factory"
-           "Function"
-           "get"
-           "implements"
-           "import"
-           "interface"
-           "library"
-           "operator"
-           "mixin"
-           "part"
-           "set"
-           "static"
-           "typedef"))
+  (#any-of? @variable.builtin
+    "abstract" "as" "covariant" "deferred" "dynamic" "export" "external" "factory" "Function" "get"
+    "implements" "import" "interface" "library" "operator" "mixin" "part" "set" "static" "typedef"))
 
 [
   "if"
--- languages/dart/outline.scm
+++ languages/dart/outline.scm
@@ -1,28 +1,28 @@
 (class_definition
-    "class" @context
-    name: (_) @name) @item
+  "class" @context
+  name: (_) @name) @item
 
 (function_signature
-    name: (_) @name) @item
+  name: (_) @name) @item
 
 (getter_signature
-    "get" @context
-    name: (_) @name) @item
+  "get" @context
+  name: (_) @name) @item
 
 (setter_signature
-    "set" @context
-    name: (_) @name) @item
+  "set" @context
+  name: (_) @name) @item
 
 (enum_declaration
-    "enum" @context
-    name: (_) @name) @item
+  "enum" @context
+  name: (_) @name) @item
 
 (extension_declaration
-    "extension" @context
-    name: (_) @name) @item
+  "extension" @context
+  name: (_) @name) @item
 
 (static_final_declaration
-    (identifier) @name) @item
+  (identifier) @name) @item
 
 (initialized_identifier
-    (identifier) @name) @item
+  (identifier) @name) @item
